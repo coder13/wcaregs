c8870525be8351bbc2cbacff77c452bfe0644ccc
Caleb <choover11@gmail.com>
2021-04-24 22:34:33 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 import Home from './pages/Home';
 import regulations from './assets/regulationsAndGuidelines.json';
 
 function App() {
   return (
-    <div id="app">
-      <nav className="navbar is-flex" role="navigation">
-        <div className="navbar-brand is-inline-flex">
-          <div className="navbar-item" style={{display: 'block', lineHeight: '1em'}} to="/">
-            <h1 style={{margin: '0px'}}>WCA-Regs</h1>
-            <span style={{fontSize: '.65em'}}>Version: {regulations.version}</span>
+    <Router>
+      <div id="app">
+        <nav className="navbar is-flex" role="navigation">
+          <div className="navbar-brand is-inline-flex">
+            <Link className="navbar-item" style={{display: 'block', lineHeight: '1em'}} to="/">
+              <h1 style={{margin: '0px'}}>WCA-Regs</h1>
+              <span style={{fontSize: '.65em'}}>Version: {regulations.version}</span>
+            </Link>
           </div>
-        </div>
-        <div className="navbar-item field is-inline-flex stretch">
-          <div className="control stretch">
-            <input className="input" v-model="query" type="text"/>
+          <div className="navbar-item field is-inline-flex stretch">
+            <div className="control stretch">
+              <input className="input" v-model="query" type="text"/>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      <div id="page">
-        <Home />
+        <div id="page">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="*">
+                <Redirect to="/" />
+              </Route>
+            </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
